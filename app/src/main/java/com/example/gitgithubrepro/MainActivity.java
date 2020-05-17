3cc38ff54988130842604cfb09d469479c608704
Christopher Rodriguez <christopher.rodriguez@softpository.com>
2020-05-17 17:17:54 -0500

    added android module library with joke

--- app/src/main/java/com/example/gitgithubrepro/MainActivity.java
+++ app/src/main/java/com/example/gitgithubrepro/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.gitgithubrepro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.javalib.Jokes;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Jokes joke = new Jokes();
+        String myJoke = joke.ajoke;
+
+        Toast.makeText(MainActivity.this, myJoke, Toast.LENGTH_SHORT).show();
+
+
+
+
     }
 }
---
+++ javalib/src/main/java/com/example/javalib/Jokes.java
@@ -0,0 +1,18 @@
+package com.example.javalib;
+
+public class Jokes {
+
+
+    public String ajoke = "What do you call cheese that is not yours? Nacho cheese!";
+
+    public static void main(String[] args) {
+        System.out.println("This is my joke");
+
+
+    }
+
+    public void tellMeAJoke() {
+
+
+    }
+}
